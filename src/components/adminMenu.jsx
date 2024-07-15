54d71c45a5968b002ca382815b236c7804d3fbd7
ChandanVasu <chandanyadavparas@gmail.com>
2024-07-15 15:43:37 +0530

    first commit

---
+++ src/components/adminMenu.jsx
@@ -0,0 +1,33 @@
+import { FaHome } from 'react-icons/fa';
+
+export default function AdminMenu() {
+  return (
+    <div className=''>
+      <ul className='adminMenuList'>
+        <li>
+          <FaHome /> <a href="/">Dashboard</a>
+        </li>
+        <li>
+          <FaHome /> <a href="/">Dashboard</a>
+        </li>
+        <li>
+          <FaHome /> <a href="/">Dashboard</a>
+        </li>
+        <li>
+          <FaHome /> <a href="/">Dashboard</a>
+        </li>      <li>
+          <FaHome /> <a href="/">Dashboard</a>
+        </li>
+        <li>
+          <FaHome /> <a href="/">Dashboard</a>
+        </li>
+        <li>
+          <FaHome /> <a href="/">Dashboard</a>
+        </li>
+        <li>
+          <FaHome /> <a href="/">Dashboard</a>
+        </li>
+      </ul>
+    </div>
+  );
+}
---
+++ src/components/header.jsx
@@ -0,0 +1,16 @@
+// components/Header.js
+"use client"
+
+
+export default function Header() {
+  return (
+    <header className=" sticky top-0 z-20 main-head py-2 px-7">
+      <div className="flex justify-between">
+        <img src="/logo.png" alt="" className="logo-img"/>
+        <div>
+        </div>
+      </div>
+
+    </header>
+  );
+}
