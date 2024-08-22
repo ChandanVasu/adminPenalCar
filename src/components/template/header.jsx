7a33047b20d7dc31422f71b3fa19c38c63a3e0f7
ChandanVasu <chandanyadavparas@gmail.com>
2024-08-22 17:39:23 +0530

    fix header

--- src/components/template/header.jsx
+++ src/components/template/header.jsx
@@ -1,21 +1,54 @@
 "use client";
-import { Avatar } from "@nextui-org/react";
+import { useState } from "react";
 import Link from "next/link";
+import AdminMenu from "@/components/template/adminMenu";
+import { FiMenu, FiX } from "react-icons/fi"; // Import icons from react-icons
 
 export default function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
-    <header className=" sticky top-0 z-999 main-head py-2 px-7 shadow-md bg-white">
+    <header className="sticky top-0 z-999 main-head py-2 px-1 md:px-7 shadow-md bg-white">
       <div className="flex justify-between items-center">
+        {/* Logo with space between */}
         <Link href="/">
-          <img src="/logo.png" alt="" className="logo-img" />
+          <img src="/logo.png" alt="Logo" className="logo-img" />
         </Link>
 
-        {/* <Avatar
-          isBordered
-          color="secondary"
-          size="sm"
-          src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
-        /> */}
+        {/* Hamburger menu on the right side */}
+        <div className="md:hidden flex items-center">
+          <button
+            className="text-gray-800 focus:outline-none"
+            onClick={toggleMenu}
+          >
+            {menuOpen ? (
+              <FiX className="w-6 h-6" /> // Close icon
+            ) : (
+              <FiMenu className="w-6 h-6" /> // Hamburger menu icon
+            )}
+          </button>
+        </div>
+      </div>
+
+      {/* Slide-in Menu */}
+      <div
+        className={`fixed inset-0 bg-black bg-opacity-50 transform ${
+          menuOpen ? "translate-x-0" : "-translate-x-full"
+        } transition-transform duration-300 ease-in-out md:hidden`}
+      >
+        <div className="bg-white w-64 h-full shadow-lg">
+          <button
+            className="text-gray-800 focus:outline-none p-4"
+            onClick={toggleMenu}
+          >
+            <FiX className="w-6 h-6" /> {/* Close icon */}
+          </button>
+          <AdminMenu />
+        </div>
       </div>
     </header>
   );
