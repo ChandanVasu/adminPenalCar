76974a882663761e6106f186d138ee37ad32670a
ChandanVasu <chandanyadavparas@gmail.com>
2024-08-31 19:22:45 +0530

    update db

--- src/lib/dbConnect.tsx
+++ src/lib/dbConnect.tsx
@@ -1,2 +1,2 @@
-export const connectionStart = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_HOST}/CarRental?retryWrites=true&w=majority`
+export const connectionStart = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_HOST}/NewCarRental?retryWrites=true&w=majority`
 
